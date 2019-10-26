c90745e1573b553ee951273fb602d722bc3c59ea
Andrés Cosín Zamarreño <Cousin183@gmail.com>
2019-10-26 14:20:11 +0200

    primer commit

---
+++ src/org/japo/java/main/Main.java
@@ -0,0 +1,52 @@
+/* 
+ * Copyright 2019 ANDRÉS COSÍN ZAMARREÑO - Cousin183@gmail.com.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.japo.java.main;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+/**
+ *
+ * @author ANDRÉS COSÍN ZAMARREÑO - Cousin183@gmail.com
+ */
+public class Main {
+
+    public static final Scanner SCN
+            = new Scanner(System.in, "Windows-1252")
+                    .useLocale(Locale.ENGLISH).useDelimiter("\\s+");
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        double altura;
+
+        try {
+            System.out.print("Introduzca su altura en metros: ");
+            altura= SCN.nextDouble();
+            
+            System.out.printf("Mi altura es....: %.2f metros%n", altura);
+            
+
+        } catch (Exception e) { 
+            System.out.println("Error de entrada!!!!!");
+            
+        } finally {
+            SCN.nextLine();
+        }
+        }
+
+    }
